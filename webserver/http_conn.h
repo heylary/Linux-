2f3af8303bb6592df9d533affeefdfe5354fe469
lary <clearcool@126.com>
2023-03-14 04:17:22 -0700

    webserver01

---
+++ webserver/http_conn.h
@@ -0,0 +1,36 @@
+#ifndef HTTPCONNECTION_H
+#define HTTPCONNECTION_H
+
+#include "http_conn.h"
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <error.h>
+#include "locker.h"
+#include <sys/uio.h>
+
+class http_conn {
+    public:
+        static int m_epollfd; //所有的socket事件都被注册到同一个epoll对象上
+        static int m_user_count; //统计用户的数量
+        http_conn() {}
+        ~http_conn() {}
+
+        void process(); //处理客户端的请求 
+    private:
+        int m_sockfd; //该http连接的socket
+        sockaddr_in m_address;  //通信的socket地址
+        
+
+};
+
+#endif
---
+++ webserver/main.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include "locker.h"
+#include "threadpool.h"
+#include <string.h>
+#include <signal.h>
+#include "http_conn.h"
+#define MAX_FD 65535   //最大的文件描述符个数   
+#define MAX_EVENT_NUMBER 10000 //一次监听的最大数
+
+//添加信号捕捉
+void addsig(int sig,void(handler)(int)) {
+    struct sigaction sa;
+    memset(&sa,'\0',sizeof(sa));
+    sa.sa_handler = handler;
+    sigfillset(&sa.sa_mask);
+    sigaction(sig,&sa,NULL);
+}
+
+//添加文件描述符到epoll中
+extern void addfd(int epollfd,int fd, bool one_shot);
+//从epoll中删除文件描述符
+extern void removefd(int epollfd,int fd);
+//从epoll中修改文件描述符
+extern void modfd(int epollfd,int fd,int ev);
+
+int main(int argc, char* argv[]) {
+    if(argc <= 1) {
+       printf("按照如下格式运行: %s port_num\n",basename(argv[0])); 
+       exit(-1);
+    }
+    //获取端口号
+    int port = atoi(argv[1]);
+    //对SIGPIPE信号处理
+    addsig(SIGPIPE,SIG_IGN);
+    //创建并初始化线程池
+    threadpool<http_conn> * pool = NULL;
+    try{
+        pool = new threadpool<http_conn>;
+    }catch(...) {
+        exit(-1);
+    }
+    // 创建一个数组用于保存所有客户端信息
+    http_conn * user = new http_conn[ MAX_FD ];
+
+    //创建监听套接字
+    int listenfd = socket(PF_INET,SOCK_STREAM,0);
+    //设置端口复用,绑定前设置
+    int reuse = 1;
+    setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse));
+    //绑定
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+    bind(listenfd,(struct sockaddr*)&address,sizeof(address));
+    //监听
+    listen(listenfd,5);
+    
+    //创建epoll对象，事件数组，多路复用
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+
+    //将监听的文件描述符添加到epoll对象中
+    addfd(epollfd,listenfd,false);
+    http_conn::m_epollfd = epollfd;
+
+    //检测是否有事件发送
+    while(true) {
+        int num = epoll_wait(epollfd,events,MAX_EVENT_NUMBER,-1);   //默认阻塞
+        if(num < 0 && (errno != EINTR)) {
+            printf("epoll failure");
+            break;
+        }
+        //循环遍历事件数组
+        for(int i = 0; i < num; i++) {
+            int sockfd = events[i].data.fd;
+            if(sockfd == listenfd) {
+                struct sockaddr_in client_address;
+                socklen_t client_addrlen = sizeof(client_address);
+                int connfd = accept(listenfd,(struct sockaddr*)&client_address,&client_addrlen);
+                if(http_conn::m_user_count >= MAX_FD) {
+                    close(connfd);
+                    continue;
+                }
+            }
+
+
+
+        }
+
+    }
+
+
+    return 0;
+}
